fa7027f10aad33b1ebeb309d3aa407ff29a5c272
Haley Zeng <hhzeng@fb.com>
2018-07-09 10:24:54 -0700

    first commit

---
+++ parsechatTests/parsechatTests.m
@@ -0,0 +1,39 @@
+//
+//  parsechatTests.m
+//  parsechatTests
+//
+//  Created by Haley Zeng on 7/9/18.
+//  Copyright © 2018 Haley Zeng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface parsechatTests : XCTestCase
+
+@end
+
+@implementation parsechatTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
